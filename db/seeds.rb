d2059da53c31d61abb16d45e01be580666244219
guillermo varela <guillovarela16@gmail.com>
2019-06-21 10:32:39 -0300

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,20 +12,20 @@ Car.destroy_all
 puts 'creating cars'
 
 Car.create!(
-  name:,
-  sku:,
-  photo1:,
-  category:,
-  brand:,
-  price:,
+  name:"lamborgini",
+  sku:"ferr",
+  photo1:"https://images.unsplash.com/photo-1536332016596-dc50468cbf41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  category:"luxury",
+  brand: "lambo",
+
   )
 Car.create!(
-  name:,
-  sku:,
-  photo1:,
-  category:,
-  brand:,
-  price:,
+  name:"Ferrari",
+  sku: "456",
+  photo1:"https://images.unsplash.com/photo-1537758239362-40eda0496bc9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80",
+  category:"exclusive",
+  brand:"ferrari",
+
   )
 # Car.create!(
 #   name:,
